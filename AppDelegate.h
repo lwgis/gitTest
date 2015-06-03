c6746f8f1ef7d66c3168fbb708ef14b8679e8aa9
lwgis <lwgis@qq.com>
2015-06-04 02:42:04 +0800

    init

---
+++ AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  GoodFarming
+//
+//  Created by 李炜 on 15/4/14.
+//  Copyright (c) 2015年 东方誉源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
